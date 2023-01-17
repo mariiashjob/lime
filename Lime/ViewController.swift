6fbebeffca996b4f5f8aeff1380ee0e4ca6a76c9
Maria Shirokova <m.shirokova@space307.com>
2023-01-17 14:41:30 +0300

    initial commit

---
+++ Lime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lime
+//
+//  Created by m.shirokova on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
